871a6b1774695d0c50a6b2f1c56c97103fed7dd3
Kevin Wang <kwangsan@gmail.com>
2019-03-27 01:00:50 -0400

    ScootMap: expose userLat, userLng, range props

--- src/components/ScootMap.js
+++ src/components/ScootMap.js
@@ -20,8 +20,11 @@ const AnyReactComponent = ({ text }) => (
 export default class ScootMap extends React.Component {
   static defaultProps = {
     center: { lat: 37.77, lng: -122.41 },
-    zoom: 11,
-    data: null
+    zoom: 12,
+    data: null,
+    userLat: 37.77,
+    userLng: -122.41,
+    range: 300
   };
 
   render() {
